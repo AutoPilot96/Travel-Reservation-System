b9e7f0868ea99f3ed5143c1e77695140562bfb0a
Pak_Man926 <davidkamau926@gmail.com>
2024-07-25 00:36:54 +0300

    Second commit.

--- Scheduling.java
+++ Scheduling.java
@@ -1,30 +1,36 @@
+import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-class Scheduling
+public class Scheduling extends Booking
 {
-    PriorityQueue<Integer> pQueue = new PriorityQueue<>();
+    PriorityQueue <Integer> pQueue = new PriorityQueue<>();
     Scanner obj = new Scanner(System.in);
 
-    int bookingID;
+    int bookingId;
 
     public void addSchedule()
     {
         String continueAdding;
-
         do
         {
-          System.out.println("Enter your Booking ID:");
-          bookingID = obj.nextInt();
-
-          pQueue.add(bookingID);
+            System.out.println("Enter your Booking ID:");
+            bookingId = obj.nextInt();
 
             System.out.println("Do you want to add another booking? (yes/no):");
             obj.nextLine();
             continueAdding = obj.nextLine();
         }
-        while(continueAdding.equalsIgnoreCase("yes"));
+        while (continueAdding.equalsIgnoreCase("yes"));
     }
 
+    public void getSchedule()
+    {
+        Iterator iterator = pQueue.iterator();
 
+        while (iterator.hasNext())
+        {
+            System.out.println(iterator.next() + " ");
+        }
+    }
 }
